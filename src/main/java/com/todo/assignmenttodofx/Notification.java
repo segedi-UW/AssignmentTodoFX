8108ec5a87bfa2ec41f232411f729289258fbc44
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2021-09-28 16:48:32 -0500

    update 3.5.6

--- src/main/java/com/todo/assignmenttodofx/Notification.java
+++ src/main/java/com/todo/assignmenttodofx/Notification.java
@@ -4,6 +4,8 @@ import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -11,12 +13,21 @@ import javafx.stage.StageStyle;
 import javafx.util.Duration;
 import org.controlsfx.control.Notifications;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Notification {
 
     private static Stage stage;
 
+    private static final ConcurrentLinkedQueue<Notification> showing = new ConcurrentLinkedQueue<>();
+    private static final Timer remover = new Timer(true);
     private final Notifications notification;
     private final Type type;
+    private final String title;
+    private final String text;
+    private long autoHide;
 
     public static void createAndShowStage() {
         Stage s = new Stage();
@@ -42,13 +53,30 @@ public class Notification {
         if (stage == null || !stage.isShowing())
             throw new IllegalStateException("Cannot create Notification before creating and showing Stage " +
                     "use the createAndShowStage() method before creating Notifications.");
+        this.title = title;
+        this.text = text;
         this.type = type;
         notification = Notifications.create();
         notification.darkStyle().position(Pos.BOTTOM_RIGHT).title(title).text(text).owner(stage);
+        setHideAfterSeconds(3);
+        Notifications threshNotification = Notifications.create();
+        threshNotification.darkStyle().position(Pos.BOTTOM_RIGHT).title("Notifications").text("Click to See Description").owner(stage);
+        threshNotification.onAction(event -> {
+            StringBuilder builder = new StringBuilder();
+            for (Notification n : showing) {
+                builder.append(n.title);
+                builder.append("\n");
+                builder.append(n.text);
+                builder.append("\n\n");
+            }
+            Platform.runLater(() -> new ThresholdAlert(builder.toString()).show());
+        });
+        notification.threshold(3, threshNotification);
     }
 
     public void setHideAfterSeconds(double duration) {
         notification.hideAfter(Duration.seconds(duration));
+        autoHide = 3;
     }
 
     public void show() {
@@ -58,6 +86,8 @@ public class Notification {
     private void showOfType() {
         // runs within the javafx application thread
         Stage main = App.getStage();
+        showing.add(this);
+        remover.schedule(removeTask(), autoHide * 1000L);
         if (main.isShowing())
             notification.owner(main);
         switch (type) {
@@ -79,8 +109,29 @@ public class Notification {
         }
     }
 
+    private TimerTask removeTask() {
+        Notification current = this;
+        return new TimerTask() {
+            @Override
+            public void run() {
+                showing.remove(current);
+            }
+        };
+    }
+
     @Override
     public String toString() {
         return notification.toString();
+    }
+
+    private static class ThresholdAlert extends Alert {
+        public ThresholdAlert(String areaText) {
+            super(AlertType.INFORMATION);
+            setTitle("AssignmentTodo Notifications");
+            setHeaderText("Notification Summary");
+            TextArea area = new TextArea(areaText);
+            area.setEditable(false);
+            getDialogPane().setContent(area);
+        }
     }
 }
